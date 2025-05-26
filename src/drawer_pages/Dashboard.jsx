6b4d693e4c8e15ad2f6f3e0375e9bb5df1f4ed34
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-05-26 10:13:49 +0530

    add summery using paper component

--- src/drawer_pages/Dashboard.jsx
+++ src/drawer_pages/Dashboard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import DashbordPaper from '../component/DashbordPaper';
+
+
+ 
 
 export default function Dashboard() {
   return (
-    <Box>This Is Dashboard Page</Box>
+    <Box>
+      <Box>
+        
+        {/* Paper  */}
+        <DashbordPaper/>
+            
+
+      </Box>
+
+
+
+
+    </Box>
   )
 }
