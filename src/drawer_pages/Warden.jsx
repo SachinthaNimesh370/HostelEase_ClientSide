a85d3b0121c40dc3059e008bb6f3ac440a6b687f
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-06-21 18:26:19 +0530

    Sign Out Process Working

---
+++ src/drawer_pages/Warden.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+export default function Warden() {
+  return (
+    <Box>
+      <h2>Warden Page</h2>
+      <p>This is the Warden page.</p>
+    </Box>
+  );
+}
